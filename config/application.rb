c0d5e12f06c56ec8e413981d966de1a9e77c8cbe
Cody Burgess <thisismyscene@gmail.com>
2020-10-26 15:35:16 -0400

    rails updates

--- config/application.rb
+++ config/application.rb
@@ -1,23 +1,13 @@
 require File.expand_path('../boot', __FILE__)
+
 require 'rails/all'
-
-# Pick the frameworks you want:
-# require "active_record/railtie"
-# require "action_controller/railtie"
-# require "action_mailer/railtie"
-# require "sprockets/railtie"
-# require "rails/test_unit/railtie"
-
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-# Bundler.require(*Rails.groups)
-Bundler.require(:default, Rails.env)
+Bundler.require(*Rails.groups)
 
 module Fotinisrestaurant
   class Application < Rails::Application
-  config.autoload_paths += %W(#{config.root}/lib)
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,56 @@
-Fotinisrestaurant::Application.routes.draw do
-  root "pages#index"
-  get "pages/home" => "pages#index", as: "home"
-  get "pages/menu" => "pages#menu", as: "menu"
-  get "pages/bar" => "pages#bar", as: "bar"
-  get "pages/about" => "pages#about", as: "about"
-  get "pages/history" => "pages#history", as: "history"
-  get "pages/contact" => "pages#contact", as: "contact"
-  get "/sitemap.xml" => "sitemap#sitemap", as: "sitemap"
-  post "pages/contact" => "pages#parse_contact"
+Rails.application.routes.draw do
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+  # root 'welcome#index'
+
+  # Example of regular route:
+  #   get 'products/:id' => 'catalog#view'
+
+  # Example of named route that can be invoked with purchase_url(id: product.id)
+  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+
+  # Example resource route (maps HTTP verbs to controller actions automatically):
+  #   resources :products
+
+  # Example resource route with options:
+  #   resources :products do
+  #     member do
+  #       get 'short'
+  #       post 'toggle'
+  #     end
+  #
+  #     collection do
+  #       get 'sold'
+  #     end
+  #   end
+
+  # Example resource route with sub-resources:
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
+
+  # Example resource route with more complex sub-resources:
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get 'recent', on: :collection
+  #     end
+  #   end
+
+  # Example resource route with concerns:
+  #   concern :toggleable do
+  #     post 'toggle'
+  #   end
+  #   resources :posts, concerns: :toggleable
+  #   resources :photos, concerns: :toggleable
+
+  # Example resource route within a namespace:
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
 end
