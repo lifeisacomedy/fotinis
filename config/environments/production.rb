0f0fa912e3ef7e380c60df80e7a46842da3753f8
Cody Burgess <thisismyscene@gmail.com>
2020-10-26 16:29:58 -0400

    Revert "FIXING?"
    This reverts commit 1e9c131b7a6869254921b4976971a2e62e5140a3.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this). CHANGED ASSETS TO FILES
-  config.serve_static_assets = false
+  config.serve_static_files = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
